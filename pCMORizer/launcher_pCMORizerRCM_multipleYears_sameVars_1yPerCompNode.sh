9f4e160b1b96c01cc1bb32ab8902e3f30021214a
Klaus Goergen <k.goergen@fz-juelich.de>
2022-11-13 20:42:08 +0100

    clean up further, retain only the latest code

--- pCMORizer/launcher_pCMORizerRCM_multipleYears_sameVars_1yPerCompNode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1996..2005} ; do
-
-  export Y=$i && sbatch --export=ALL,Y=$Y --job-name=pCMORizerWRF$Y WRF_CMORizer.sh.lw2
-
-done
--- pack.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tar -cvzf CMORizer.$(date +"%Y%m%d%H%M%S").tgz pack.sh load_env Makefile WRF_CMORizer.f90 *.nml* *sbatch*
