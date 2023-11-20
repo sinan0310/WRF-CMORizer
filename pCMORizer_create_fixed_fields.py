ba07865777caf060ca8b3896d80a5a8c5c4f7a7c
Klaus Goergen (KGo) <k.goergen@fz-juelich.de>
2023-11-20 13:43:53 +0100

    adjust the fx generator, generalize and test, some smaller fixes and adjustments, use the ensemble ID of the driving model, this is also allowed and more consistent, instead of r0i0p0

--- pCMORizer_create_fixed_fields.py
+++ pCMORizer_create_fixed_fields.py
@@ -1,13 +1,26 @@
-import numpy as np
-import xarray as xa
-import cftime
-import sys
-import os
-import f90nml
-from glob import glob
-#from pyproj import Geod
+# -*- coding: utf-8 -*-                                                         
 
-# read namelist file
+""" Part of CMORization: Generate te fx netCDF files, CORDEX-FPSCONV specs. """         
+
+import os
+
+import numpy as np
+import xarray as xr
+import f90nml
+
+__author__ = "Heimo TRUHETZ, HTr, Wegener-Center/Uni Graz"
+__copyright__ = "Copyright 2023"
+__credits__ = [""]
+__license__ = "MIT"
+__version__ = "20231116"
+__maintainer__ = "Klaus GOERGEN, KGo"
+__email__ = "k.goergen@fz-juelich.de"
+__status__ = "Release"
+
+################################################################################
+# read CMORizer namelist and static field raw data
+################################################################################
+
 nml = f90nml.read('runctrl.current.nml_template_d02_DA')
 
 xoffset = nml['model_config']['xoffset']
@@ -15,43 +28,36 @@ xfocus = nml['model_config']['xfocus']
 yoffset = nml['model_config']['yoffset']
 yfocus = nml['model_config']['yfocus']
 
-DS = xa.open_dataset(nml['static_fields']['pnfngeo'])
+DS = xr.open_dataset(nml['static_fields']['pnfngeo'])
 POLE_LAT = DS.attrs['POLE_LAT']
 POLE_LON = DS.attrs['POLE_LON']
 if POLE_LON > 0.0:
     POLE_LON = POLE_LON - 180.0
 
-# orog
+################################################################################
+# orog, Surface Altitude, [m]
+################################################################################
 
-# file name
-
+# r0i0p0 is not mandatory and less consistent
 dir_name = nml['globalvars']['project_id'] + '/' + nml['globalvars']['product'] + '/' + \
     nml['globalvars']['cordex_domain'] + '/' + nml['globalvars']['institute_id'] + '/' + \
     nml['globalvars']['driving_model_id'] + '/' + nml['globalvars']['experiment_id'] + '/' + \
-    'r0i0p0' + '/' + nml['globalvars']['model_id'] + '/' + \
+    nml['globalvars']['driving_model_ensemble_member'] + '/' + nml['globalvars']['model_id'] + '/' + \
     nml['globalvars']['rcm_version_id'] + '/' + 'fx' + '/'
 
+print(dir_name)
 
 orog = DS[['HGT_M','XLAT_M','XLONG_M','CLAT','CLONG','MAPFAC_M']].isel(west_east=slice(xoffset,xoffset+xfocus),
         south_north=slice(yoffset,yoffset+yfocus)).rename({'HGT_M':'orog', 'XLAT_M':'lat', 'XLONG_M':'lon', 
             'CLAT':'rlat', 'CLONG':'rlon'}).squeeze()
 
-#corners = DS[['XLAT_C','XLONG_C']].isel(west_east_stag=slice(xoffset,xoffset+xfocus+1), 
-#        south_north_stag=slice(yoffset,yoffset+yfocus+1)).squeeze()
-
-#U = DS[['XLAT_U','XLONG_U']].isel(west_east_stag=slice(xoffset,xoffset+xfocus+1), 
-#        south_north=slice(yoffset,yoffset+yfocus)).squeeze()
-#V = DS[['XLAT_V','XLONG_V']].isel(west_east=slice(xoffset,xoffset+xfocus), 
-#        south_north_stag=slice(yoffset,yoffset+yfocus+1)).squeeze()
-
-
-ds = xa.Dataset(
+ds = xr.Dataset(
         data_vars=dict(
             orog=(["rlat", "rlon"],orog.orog.values,{ 
                 "standard_name": "surface_altitude",
                 "long_name": "Surface Altitude",
                 "units": "m",
-                "coordinates": "lon lat",
+                "coordinates": "lat lon",
                 "grid_mapping": "rotated_pole",
                 "missing_value": np.float32(1.e+20),
                 "_FillValue": 1.e+20
@@ -71,7 +77,7 @@ ds = xa.Dataset(
                 "axis": "Y"
                  }
             )
-            ),
+        ),
         coords=dict(
             lon=(["rlat", "rlon"],np.double(orog.lon.values),{
                 "standard_name": "longitude",
@@ -85,7 +91,7 @@ ds = xa.Dataset(
                 "units": "degrees_north"
                 }
             )
-            ),
+        ),
         attrs={
             "Conventions": nml['globalvars']['conventions'],
             "conventionsURL": nml['globalvars']['conventionsURL'],
@@ -106,7 +112,7 @@ ds = xa.Dataset(
             "CORDEX_domain": nml['globalvars']['CORDEX_domain'],
             "product": nml['globalvars']['product'],
             "references": nml['globalvars']['references'],
-            "tracking_id": os.popen("uuidgen -t").read()[0:36],
+            "tracking_id": os.popen("uuidgen").read()[0:36],
             "title": nml['globalvars_additional']['title'],
             "comment": nml['globalvars_additional']['comment'],
             "nesting_levels": nml['globalvars_additional']['nesting_levels'],
@@ -116,10 +122,11 @@ ds = xa.Dataset(
             }
         )
 
- 
 f_name = 'orog_' + nml['globalvars']['cordex_domain'] + '_' + nml['globalvars']['driving_model_id'] + '_' + \
-    nml['globalvars']['experiment_id'] + '_' + 'r0i0p0' + '_' + nml['globalvars']['model_id'] + '_' + \
+    nml['globalvars']['experiment_id'] + '_' + nml['globalvars']['driving_model_ensemble_member'] + '_' + nml['globalvars']['model_id'] + '_' + \
     nml['globalvars']['rcm_version_id'] + '_' + 'fx' + '.nc'
+
+print(f_name)
 
 os.system("mkdir -p " + dir_name + 'orog/')
 
@@ -129,23 +136,22 @@ ds.to_netcdf(dir_name + 'orog/' + f_name, mode = 'w', format = 'NETCDF4_CLASSIC'
     'rlat': {'_FillValue': None},
     'lon' : {'_FillValue': None},
     'lat' : {'_FillValue': None}
-#    'rotated_pole': {'dtype':'str'} 
+    #'rotated_pole': {'dtype':'str'} 
     })
 
 ds.close()
 
-# areacella
+################################################################################
+# areacella, Atmosphere Grid-Cell Area, [m2]
+################################################################################
 
 ds = ds.rename({'orog':'areacella'})
 
-#print(ds)
-
-DX = orog.attrs['DX']   # grid spacing in m
-DY = orog.attrs['DY']   # grid spacing in m
+DX = orog.attrs['DX']  # grid spacing in m
+DY = orog.attrs['DY']  # grid spacing in m
 
 ds.areacella.values = np.float32(DX*DY/orog.MAPFAC_M.values)
 
-# modify attributes
 ds.areacella.attrs = { 
     "standard_name": "cell_area",
     "long_name": "Atmosphere Grid-Cell Area",
@@ -156,10 +162,11 @@ ds.areacella.attrs = {
     "_FillValue": 1.e+20
     }
 
-
 f_name = 'areacella_' + nml['globalvars']['cordex_domain'] + '_' + nml['globalvars']['driving_model_id'] + '_' + \
-    nml['globalvars']['experiment_id'] + '_' + 'r0i0p0' + '_' + nml['globalvars']['model_id'] + '_' + \
+    nml['globalvars']['experiment_id'] + '_' + nml['globalvars']['driving_model_ensemble_member'] + '_' + nml['globalvars']['model_id'] + '_' + \
     nml['globalvars']['rcm_version_id'] + '_' + 'fx' + '.nc'
+
+print(f_name)
 
 os.system("mkdir -p " + dir_name + 'areacella/')
 
@@ -174,4 +181,10 @@ ds.to_netcdf(dir_name + 'areacella/' + f_name, mode = 'w', format = 'NETCDF4_CLA
 
 ds.close()
 
+################################################################################
+# sftlf, Land Area Fraction, [%]
+################################################################################
 
+#sftgif, Fraction of Grid Cell Covered with Glacier, [%]
+#mrsofc, Capacity of Soil to Store Water, [kg m-2]
+#rootd, Maximum Root Depth, [m]
