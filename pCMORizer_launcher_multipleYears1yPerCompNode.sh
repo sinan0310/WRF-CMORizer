81f833f478ea316cdd5d781d38853c9dd7cf7620
Klaus GOERGEN <k.goergen@fz-juelich.de>
2023-11-21 16:46:13 +0100

    adjust for BB run proccessing, make a bit more generic / user friendly, 2nd attempt

--- pCMORizer_launcher_multipleYears1yPerCompNode.sh
+++ pCMORizer_launcher_multipleYears1yPerCompNode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # start each year on a seperate compute node
-for i in {1996..2005} ; do
+for i in {2000..2014} ; do
 
   # launch d01 and d02 at the same time on same compute node, but only one set of vars -- previous default
   #export Y=$i && sbatch --export=ALL,Y=$Y --job-name=pCMORizer$Y pCMORizer_runctrl_singleVarTypes_twoDomains.sh
 
-  # launch d01 and d02 at the same time on different compute nodes and all vars at the same time
+  # launch d01 and d02 at the same time on different compute nodes and all vars for one year at the same time on one compute node -- 2023 default
   #export Y=$i && export DOM=d01 && sbatch --export=ALL,Y=$Y,DOM=$DOM --job-name=pCMORizer$Y$DOM pCMORizer_runctrl_allVarTypes_singleDomain.sh
   export Y=$i && export DOM=d02 && sbatch --export=ALL,Y=$Y,DOM=$DOM --job-name=pCMORizer$Y$DOM pCMORizer_runctrl_allVarTypes_singleDomain.sh
 
--- pCMORizer_runctrl_allVarTypes_singleDomain.sh
+++ pCMORizer_runctrl_allVarTypes_singleDomain.sh
@@ -11,7 +11,7 @@
 #SBATCH --disable-turbomode
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
-###SBATCH --constraint=largedata
+#SBATCH --constraint=largedata
 
 # AUTHOR(S): Heimo TRUHETZ (HTr), Uni-Graz/WEGC, heimo.truhetz@uni-graz.at, Klaus GOERGEN (KGo), FZJ/IBG-3, k.goergen@fz-juelich.de
 # VERSION: 2023-11-04
@@ -27,8 +27,16 @@ echo $SLURM_JOB_NAME
 let Y=$Y
 DOM=$DOM
 
-#dir_data_in="/p/largedata/jjsc39/jjsc3900/sim/CORDEX-FPSCONV_EUR-15-ALP-3_SMHI-EC-EARTH_historical_r12_FZJ-IDL-WRF381DA_v00aJurecaDcCpuProdPrjTt19952005/simres"
-dir_data_in="/p/scratch/cjjsc39/goergen1/sim/tmp_FPSCONV/tmp_DA/simres"
+# ADJUST
+identifier="BB" # BB CA DA
+#dir_data_in="/p/scratch/cjjsc39/goergen1/sim/tmp_FPSCONV/tmp_DA/simres"
+dir_data_in="/p/largedata/jjsc39/jjsc3900/sim/CORDEX-FPSCEM_EUR-15-ALP-3_ECMWF-ERAINT_evaluation_r1i1p1_FZJ-IBG3-WRF381BB_v03aJurecaCpuProdTt20002014/simres"
+
+# ADJUST, this is for cape and cin only, needs to be in line with the runctrl-namelists, different for d01 and d02
+#dir_special_var="/p/scratch/cjjsc39/goergen1/sim/tmp_FPSCONV/tmp_DA/postpro/CMORized/CORDEX-FPSCONV/output/ALP-3/FZJ-IDL/SMHI-EC-EARTH/historical/r12i1p1/FZJ-IDL-WRF381DA/fpsconv-x1n2-v1/1hr"
+dir_special_var="/p/scratch/cjjsc39/goergen1/sim/tmp_FPSCONV/tmp_BB/postpro/CMORized/CORDEX-FPSCONV/output/ALP-3/FZJ-IDL/ECMWF-ERAINT/evaluation/r1i1p1/FZJ-WRF381BB/fpsconv-x1n2-v1/1hr"
+#fn_special_var="_ALP-3_SMHI-EC-EARTH_historical_r12i1p1_FZJ-IDL-WRF381DA_fpsconv-x1n2-v1_1hr_"
+fn_special_var="_ALP-3_ECMWF-ERAINT_evaluation_r1i1p1_FZJ-WRF381BB_fpsconv-x1n2-v1_1hr_"
 
 dir_work=$(pwd)
 mkdir -p ${dir_work}/${DOM}/${Y}
@@ -46,10 +54,10 @@ ln -sf ${dir_data_in}/${DOM}/*/wrfxtrm_${DOM}_${Y}* .
 
 # ADJUST depending on the content of the outputs, one more file needs to be linked here in addition in order to being able to calculate the means (-12-31_23:30), here <year+1>-01-01_00:00 is needed
 # ADJUST do not use this when just testing for a single month, the tool finds the additional file and does a processing, i.e. creates a new netCDF file and adds the data from the linked file below
-if [ "$Y" != "2005" ]
-then
+#if [ "$Y" != "2005" ]
+#then
   ln -sf ${dir_data_in}/${DOM}/*/wrfout_${DOM}_${Yn}0101* .
-fi
+#fi
 
 # ADJUST depending on the variables, set the nvar and link filenames and adjust the wall clock time
 cp -f ../../runctrl.vars.*.nml .
@@ -63,7 +71,7 @@ log="/dev/zero"
 
 # std sfc
 let nvar=39
-cp -f ../../runctrl.current.nml_template_${DOM}_DA runctrl.current.nml_${DOM}
+cp -f ../../runctrl.current.nml_template_${DOM}_${identifier} runctrl.current.nml_${DOM}
 sed -i "s/__YYYY__/$Y/g" runctrl.current.nml_${DOM}
 sed -i "s/__nvar__/$nvar/g" runctrl.current.nml_${DOM}
 ln -sf runctrl.current.nml_${DOM} runctrl.current.nml
@@ -73,7 +81,7 @@ sleep 30
 
 # std presslev
 let nvar=36
-cp -f ../../runctrl.current.nml_template_${DOM}_DA runctrl.current.nml_${DOM}
+cp -f ../../runctrl.current.nml_template_${DOM}_${identifier} runctrl.current.nml_${DOM}
 sed -i "s/__YYYY__/$Y/g" runctrl.current.nml_${DOM}
 sed -i "s/__nvar__/$nvar/g" runctrl.current.nml_${DOM}
 ln -sf runctrl.current.nml_${DOM} runctrl.current.nml
@@ -83,7 +91,7 @@ sleep 30
 
 # std minmax
 let nvar=2
-cp -f ../../runctrl.current.nml_template_${DOM}_DA runctrl.current.nml_${DOM}
+cp -f ../../runctrl.current.nml_template_${DOM}_${identifier} runctrl.current.nml_${DOM}
 sed -i "s/__YYYY__/$Y/g" runctrl.current.nml_${DOM}
 sed -i "s/__nvar__/$nvar/g" runctrl.current.nml_${DOM}
 ln -sf runctrl.current.nml_${DOM} runctrl.current.nml
@@ -93,7 +101,7 @@ sleep 30
 
 ## special serial variant
 #let nvar=2
-#cp -f ../../runctrl.current.nml_template_${DOM}_DA runctrl.current.nml_${DOM}
+#cp -f ../../runctrl.current.nml_template_${DOM}_${identifier} runctrl.current.nml_${DOM}
 #sed -i "s/__YYYY__/$Y/g" runctrl.current.nml_${DOM}
 #sed -i "s/__nvar__/$nvar/g" runctrl.current.nml_${DOM}
 #ln -sf runctrl.current.nml_${DOM} runctrl.current.nml
@@ -108,7 +116,7 @@ for MM in `seq -w 1 12`; do
   mkdir -p ${Y}_${MM}
   cd ${Y}_${MM}
 	DD=`date --date="$Y/$MM/1 + 1 month - 1 day" "+%d"`
-  cp -f ../../runctrl.current.nml_template_${DOM}_DA runctrl.current.nml_${DOM}
+  cp -f ../../runctrl.current.nml_template_${DOM}_${identifier} runctrl.current.nml_${DOM}
   # not needed to do the individual aggregation, the filelist determines for the tool what to work on
   # yearly aggregation is still on -> this determines also the type of file which is created
   # not needed to concatenate anything if the timespan is long enough for the initial file to be created  before the next process wants to access
@@ -128,15 +136,16 @@ done
 
 wait
 
-# special accelerated variant, months are concatenated, dirty hack
-#find . -name "*${Yn}0101*" -delete
+# special accelerated variant, months are concatenated
+# find . -name "*${Yn}0101*" -delete
+# 
 declare -a var_arr=("cape" "cin")
 for var in "${var_arr[@]}"; do
-	cd /p/scratch/cjjsc39/goergen1/sim/tmp_FPSCONV/tmp_DA/postpro/CMORized/CORDEX-FPSCONV/output/ALP-3/FZJ-IDL/SMHI-EC-EARTH/historical/r12i1p1/FZJ-IDL-WRF381DA/fpsconv-x1n2-v1/1hr/${var}
-	ncrcat -h ${var}_ALP-3_SMHI-EC-EARTH_historical_r12i1p1_FZJ-IDL-WRF381DA_fpsconv-x1n2-v1_1hr_${Y}*.nc tmp.nc
+	cd ${dir_special_var}/${var}
+	ncrcat -h ${var}${fn_special_var}${Y}*.nc tmp.nc
 	[ $? -ne 0 ] && exit 1
-	rm ${var}_ALP-3_SMHI-EC-EARTH_historical_r12i1p1_FZJ-IDL-WRF381DA_fpsconv-x1n2-v1_1hr_${Y}*.nc
-	mv tmp.nc ${var}_ALP-3_SMHI-EC-EARTH_historical_r12i1p1_FZJ-IDL-WRF381DA_fpsconv-x1n2-v1_1hr_${Y}010100-${Y}123123.nc
+	rm ${var}${fn_special_var}${Y}*.nc
+	mv tmp.nc ${var}${fn_special_var}${Y}010100-${Y}123123.nc
 done
 
 # if not run as a single run, or as part of the launcher script, one can 
