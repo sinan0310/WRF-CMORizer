7e159df26a900ef731d0179d4bf99abd382642a5
Yoselita <milovacj@unican.es>
2025-04-12 00:50:22 +0200

    Update fix_vaXm_wind.py
    Cut out the relaxation zone from vars sinalpha and cosalpha in geo_em
    file to fit the postprocessed file fixed. User need to provide number
    of grids in the relaxation zone that is cut out when postprocessing
    the files.

--- fix_vaXm_wind.py
+++ fix_vaXm_wind.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import shutil
 
-def correct_va_wind_component(ua_file_path, va_wrong_file_path, geo_em_path):
+def correct_va_component(ua_file_path, va_wrong_file_path, geo_em_path, xoffset, yoffset):
     """
     Corrects the va component at the specified height (m). 
     It uses ua from one file and va_wrong from another, and corrects va 
@@ -13,17 +13,20 @@ def correct_va_wind_component(ua_file_path, va_wrong_file_path, geo_em_path):
     masking out the relaxation zone as the geo_em file give the complete domain.
 
     Parameters:
-        ua_file (str): Path to the postprocessed NetCDF file with correct ua.
-        va_wrong_file (str): Path to the file with incorrect va.
-        geo_em (str): Path to the geo_em file.
+        ua_file_path (str): Path to the postprocessed NetCDF file with correct ua.
+        va_wrong_file_path (str): Path to the file with incorrect va.
+        geo_em_path (str): Path to the geo_em file.
+        xoffset (int): Number of the grids in x direction in the relaxation zone
+        yoffset (int): Number of the grids in y direction the relaxation zone
         
     Returns:
-        va_wrong_file with corrected va_values
+        va_corrected (xarray.DataArray): Corrected va variable.
         
     To run:
-        python fix_vaXm_wind.py ua_file va_wrong_file geo_em
+        python fix_vaXm_wind.py ua_file va_wrong_file geo_em xoffset {yoffset}
+        NOTE: yoffset not obligatory if xoffset=yoffset
     """
-
+        
     # Load ua from correct file
     ds_ua = xr.open_dataset(ua_file_path)
     dirname, basename = os.path.split(ua_file_path)
@@ -40,32 +43,45 @@ def correct_va_wind_component(ua_file_path, va_wrong_file_path, geo_em_path):
 
     # Load geo_em fields
     ds_geo = xr.open_dataset(geo_em_path)
-    sinalpha = ds_geo['SINALPHA'].squeeze()
-    cosalpha = ds_geo['COSALPHA'].squeeze()
-    xlat = ds_geo['XLAT_M'].squeeze()
-    xlon = ds_geo['XLONG_M'].squeeze()
+    if xoffset > 0 and yoffset > 0:
+        sinalpha = ds_geo['SINALPHA'].squeeze()[yoffset:-(yoffset+2), xoffset:-(xoffset+2)]
+        cosalpha = ds_geo['COSALPHA'].squeeze()[yoffset:-(yoffset+2), xoffset:-(xoffset+2)]
+        xlat = ds_geo['XLAT_M'].squeeze()[yoffset:-(yoffset+2), xoffset:-(xoffset+2)]
+        xlon = ds_geo['XLONG_M'].squeeze()[yoffset:-(yoffset+2), xoffset:-(xoffset+2)]
+    elif xoffset > 0 and yoffset == 0:
+        sinalpha = ds_geo['SINALPHA'].squeeze()[yoffset:-(yoffset+2), :]
+        cosalpha = ds_geo['COSALPHA'].squeeze()[yoffset:-(yoffset+2), :]
+        xlat = ds_geo['XLAT_M'].squeeze()[yoffset:-(yoffset+2), :]
+        xlon = ds_geo['XLONG_M'].squeeze()[yoffset:-(yoffset+2), :]
+    elif xoffset == 0 and yoffset > 0:
+        sinalpha = ds_geo['SINALPHA'][:, xoffset:xoffset-2]
+        cosalpha = ds_geo['COSALPHA'][:, xoffset:xoffset-2]
+        xlat = ds_geo['XLAT_M'][:, xoffset:xoffset-2]
+        xlon = ds_geo['XLONG_M'][:, xoffset:xoffset-2]
+    else:
+        sinalpha = ds_geo['SINALPHA'].squeeze()
+        cosalpha = ds_geo['COSALPHA'].squeeze()
+        xlat = ds_geo['XLAT_M'].squeeze()
+        xlon = ds_geo['XLONG_M'].squeeze()
+        
+    # Check if raw and postprocessed domain mach
+    if (lat.shape != xlat.shape) or (lon.shape != xlon.shape) \
+        or not np.isclose(lat.values[0, 0], xlat.values[0, 0], atol=1e-4) \
+        or not np.isclose(lon.values[0, 0], xlon.values[0, 0], atol=1e-4):
+        print(lat.values[0, 0])
+        print(xlat.values)
+        
+        print("Error: Shape or value mismatch between raw and postprocessed domain.")
+        print("Check if the numebr of grids in the relaxation zone is correct.")
+        print(f"{lat.shape},{xlat.shape},{lat.values[0, 0]},{xlat.values[0, 0]}, {lon.values[0, 0]},{xlon.values[0, 0]}")
+        sys.exit(1)  # Exit the script with an error code
+        
+    else:
+        print("lat, lon, xlat, xlon shapes match and values at (0, 0) are equal. Proceeding with the process.")
 
-    # Find overlapping region between geo_em and postprocessed domain
-    lat_min, lat_max = float(lat.min()), float(lat.max())
-    lon_min, lon_max = float(lon.min()), float(lon.max())
-
-    mask_lat = (xlat >= lat_min) & (xlat <= lat_max)
-    mask_lon = (xlon >= lon_min) & (xlon <= lon_max)
-    domain_mask = mask_lat & mask_lon
-
-    idx_i, idx_j = np.where(domain_mask)
-    i_min, i_max = idx_i.min(), idx_i.max()
-    j_min, j_max = idx_j.min(), idx_j.max()
-
-    # Slice SINALPHA and COSALPHA to match the postprocessed domain
-    sinalpha_cut = sinalpha.isel(south_north=slice(i_min, i_max + 1),
-                                 west_east=slice(j_min, j_max + 1))
-    cosalpha_cut = cosalpha.isel(south_north=slice(i_min, i_max + 1),
-                                 west_east=slice(j_min, j_max + 1))
-                                 
-    # Fill the array - from 2D to 3D
-    sinalpha = np.broadcast_to(sinalpha_cut, va_wrong.shape)
-    cosalpha = np.broadcast_to(cosalpha_cut, va_wrong.shape)
+    # Fill 2D variables with 3rd dimension, to fit the shapes of ua and va_wrong
+    sinalpha = np.broadcast_to(sinalpha, va_wrong.shape)
+    cosalpha = np.broadcast_to(sinalpha, va_wrong.shape)
 
     # Compute corrected components
     denom = sinalpha + cosalpha
@@ -75,32 +91,38 @@ def correct_va_wind_component(ua_file_path, va_wrong_file_path, geo_em_path):
     
     return va_corrected
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python fix_vWind_interpolation.py <ua_file> <va_wrong_file> <geo_em_file>")
+    # Arguments
+    if len(sys.argv) < 5:
+        print("Usage: python fix_vaXm_wind.py [ua_file] [va_wrong_file] [geo_em_file] [xoffset]")
+        print("If xoffset != yoffset: python fix_vaXm_wind [ua_file] [va_wrong_file] [geo_em_file] [xoffset] [yoffset]")
         sys.exit(1)
+    elif len(sys.argv) == 5:
+        xoffset = int(sys.argv[4])
+        yoffset = xoffset
+    else:
+        xoffset = int(sys.argv[4])
+        yoffset = int(sys.argv[5])       
 
     ua_file = sys.argv[1]
     va_wrong_file = sys.argv[2]
     geo_em_file = sys.argv[3]
 
     # Fix va component
-    va_fixed = correct_va_wind_component(ua_file, va_wrong_file, geo_em_file)
+    va_fixed = correct_va_component(ua_file, va_wrong_file, geo_em_file, xoffset, yoffset)
     
     # Backup the wrong file. Comment out if not necessary
-    dirname, basename = os.path.split(va_wrong_file)
-    va_var = basename.split("_", 1)
+    dirname, fname = os.path.split(va_wrong_file)
+    va_var = fname.split("_", 1)    
     if len(va_var) == 2:
-        new_basename = f"{va_var[0]}wrong_{va_var[1]}"
+        new_fname = f"{va_var[0]}wrong_{va_var[1]}"
     else:
-        new_basename = f"{basename}.backup"  
-
-    backup_path = os.path.join(dirname, new_basename)
+        new_fname = f"{basename}.backup"    
+    backup_path = os.path.join(dirname, new_fname)
     shutil.copy(va_wrong_file, backup_path)
     print(f"📝 Backed up original file to: {backup_path}")
     
-    # Replace values directly in the existing NetCDF file
+    # Save into the file named as orginal
     ds_va = xr.open_dataset(va_wrong_file)
     ds_va.attrs['comment1'] = f'Derotation of {va_var[0]} corrected.'  # Add comment that the file if fixed. If not necessary commnet it out
     ds_va[f'{va_var[0]}'].data[:] = va_fixed.data                      # Replacing the values
